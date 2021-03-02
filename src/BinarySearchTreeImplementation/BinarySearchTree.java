7fb67d3217a519fcc0e7f583a43d54bd09f99281
u6943702 <pdsla112@hotmail.com>
2021-03-03 08:57:34 +1100

    Add all untested implementations of binary search tree, except for the search methods

--- src/BinarySearchTreeImplementation/BinarySearchTree.java
+++ src/BinarySearchTreeImplementation/BinarySearchTree.java
@@ -25,10 +25,89 @@ public class BinarySearchTree {
         return root;
     }
 
-//    public void delete(int key) {
-//
+    /**
+     * Deletes the relevant node (according to the key inputted by the user), using two helper functions:
+     * getNodeToDelete(int deleteKey, Node root) and getMinRight(Node nodeToDelete).
+     * @param key
+     */
+    public void delete(int key) {
+        if (root.left == null && root.right == null) {
+            if (root.key == key)
+                root = null;
+        }
+
+        Node nodeToDelete = getNodeToDelete(key, root);
+        if (nodeToDelete.right == null && nodeToDelete.left == null) {  //If the nodeToDelete is a leaf.
+            nodeToDelete = null;
+        } else if (nodeToDelete.right == null) {  //If the nodeToDelete has an empty right tree.
+            Node immediateLeftNode = nodeToDelete.left;
+            nodeToDelete.key = immediateLeftNode.key;
+            nodeToDelete.left = immediateLeftNode.left;
+            nodeToDelete.right = immediateLeftNode.right;
+        } else if (nodeToDelete.right != null) {  //If the nodeToDelete has a non-empty right tree
+            if (nodeToDelete.right.left == null) {  //If the left branch of the right branch of nodeToDelete is empty.
+                nodeToDelete.key = nodeToDelete.right.key;
+                nodeToDelete.right = nodeToDelete.right.right;
+            } else {
+                Node replacementNode = getMinRight(nodeToDelete);
+                nodeToDelete.key = replacementNode.key;
+            }
+        }
+    }
+
+    /**
+     * Returns the replacement node, and manages the branch locally in that area of the tree.
+     * @param nodeToDelete
+     * @return
+     */
+    public Node getMinRight(Node nodeToDelete) {
+        if (nodeToDelete.left == null) {
+            if (nodeToDelete.right == null) {
+                Node result = nodeToDelete;
+                nodeToDelete = null;
+                return result;
+            } else {
+                Node result = nodeToDelete;
+                nodeToDelete.key = nodeToDelete.right.key;
+                nodeToDelete.right = nodeToDelete.right.right;
+                return result;
+            }
+        } else {
+            getMinRight(nodeToDelete.left);
+        }
+        return nodeToDelete;
+    }
+
+//    public int getMinImmediateRight(Node node) {
+//        Node immediateRightNode = node.right;
+//        if (immediateRightNode.left == null) {
+//            Node rightSubTree = immediateRightNode.right;
+//            node.key = immediateRightNode.key;
+//            node.right = rightSubTree;
+//            return node.key;
+//        }
 //    }
-//
+
+    /**
+     * Assumption: the key does exist in the tree.
+     * Returns the node that the user wants to delete, based on the key that the user inputs.
+     * @param deleteKey
+     * @param root
+     * @return
+     */
+    public Node getNodeToDelete(int deleteKey, Node root) {
+        if (root.key == deleteKey) {
+            return root;
+        }
+
+        if (deleteKey < root.key) {
+            getNodeToDelete(deleteKey, root.left);
+        } else if (deleteKey > root.key) {
+            getNodeToDelete(deleteKey, root.right);
+        }
+        return root;
+    }
+
     public int getMin() {
         Node minNode = root;
         if (root.left == null) {
